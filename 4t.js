2a21b08701813d4ac78dc4699620b54f615f58c0
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-02-16 08:05:47 +0000

    v0.0.2 ⬇︎
    - removed tttt from bin property of package.json
    - executable file 4t.js is the entry point for trythistrythat

--- 4t.js
+++ 4t.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-// Splicing out from `process.argv` the paths for node and 4t.js
+// - splicing out from `process.argv` the paths for node and 4t.js
 process.argv.splice( 0, 2 )
 
-// Process name.
-process.title = 'tttt'
+// - process.title
+process.title = '4t'
