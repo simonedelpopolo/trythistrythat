ab24b72d36e45f164e24106328b902d40a316a22
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-02-16 08:14:26 +0000

    v0.0.2 ⬇︎
    the exporter.js simplify the exported names for the main file index.js that exports globally for the package itself.
    
    index.js requires better exported function and constants names.

---
+++ index.js
@@ -0,0 +1,18 @@
+import {
+    entry_point__,
+    process_exit__,
+    process_title__,
+    shell_exit_codes__,
+    stderr__
+} from './lib/exporter.js'
+
+// - Object [activity]
+export const process_exit = process_exit__
+export const stderr = stderr__
+
+// - Object [error]
+export const shell_exit_codes = shell_exit_codes__
+
+// - Object [input]
+export const entry_point = entry_point__
+export const process_title = process_title__
