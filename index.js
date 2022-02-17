c0b843c73cd7d83beb39a5c0c3030a1dd1af5379
simonedelpopolo <simonedelpopolo@outlook.com>
2022-02-17 21:00:43 +0000

    better comments and rexporting of the right types

--- index.js
+++ index.js
@@ -6,13 +6,62 @@ import {
     stderr__
 } from './lib/exporter.js'
 
-// - Object [activity]
-export const process_exit = process_exit__
-export const stderr = stderr__
+// - Object [activity]      ____
 
-// - Object [error]
+/**
+ * Exit process with message and exit code. Object [activity.exit].
+ *
+ * @param {string|Buffer} message - Required argument.
+ * @param {Error} [error_type] - Default set to Error('4t-internal error').
+ * @param {number} [exit_code=101] - Default set to 101, process exit code.
+ * @returns { void }
+ */
+export async function process_exit ( message, error_type, exit_code ) {
+    return process_exit__( message, error_type, exit_code )
+}
+
+/**
+ * Wrap to process.stderr.write. Object [activity.stderr].
+ *
+ * @param {Buffer|string} message - The message to the stderr.
+ * @returns {Promise | PromiseFulfilledResult<string> | PromiseRejectedResult<Error>}
+ */
+export async function stderr( message ) {
+    return stderr__( message )
+}
+
+// - Object [errors]            ____
+
+/**
+ * Object [errors.shell_exit_codes].
+ *
+ * @type { { commands:1, flags:2, type_checking:3, internal_error: 4} }
+ */
 export const shell_exit_codes = shell_exit_codes__
 
-// - Object [input]
-export const entry_point = entry_point__
-export const process_title = process_title__
+// - Object [input]             ____
+
+/**
+ * Object [input.entry_point].
+ * Shared entry point for the available executable files.
+ * Its return an object from the given process.argv.
+ * Object [input.process_title] elaborates commands, flags and options dispatching to the right process switching the process.title.
+ *
+ * @param {string[]} args - The process.argv passed to the process.
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Generator/return#using_return
+ * @returns {Promise | object} - return the return of the async function generator to set it "done" and free resources.
+ */
+export async function entry_point( args ) {
+    return entry_point__( args )
+}
+
+/**
+ * Switches process.title returning an object containing command, flags and options to be digested.
+ * The returned Promise always resolves.
+ *
+ * @param {{object:{[p: string]: any}, keys:string[]}} process_parsed_argv - Object.
+ * @returns {Promise<{command: {[p:object]:any}}>}
+ */
+export async function process_title( process_parsed_argv ) {
+    return process_title__( process_parsed_argv )
+}
