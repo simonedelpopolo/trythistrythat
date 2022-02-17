085b87b7c51e1f37fd3645eede0c27c88965a202
simonedelpopolo <simonedelpopolo@outlook.com>
2022-02-17 20:58:33 +0000

    testing Object [input.entry_point]

--- 4t.js
+++ 4t.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
+import { entry_point } from './index.js'
 
 // - splicing out from `process.argv` the paths for node and 4t.js
 process.argv.splice( 0, 2 )
 
 // - process.title
 process.title = '4t'
+
+/**
+ * @type {Promise|{command:{[p:object]:object}}}
+ */
+const digestive = await entry_point( process.argv )
+
+switch ( Object.entries( digestive.command )[ 0 ][ 0 ] ) {
+    
+    case  'file':
+        ( ( options ) => {console.trace( options )} )( digestive.command.test )
+        break
+}
