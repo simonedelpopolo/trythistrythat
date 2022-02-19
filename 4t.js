a0ad5f5cc407a3982772b3fcd62c8e32207f755c
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-02-19 00:23:31 +0000

    v0.0.2

--- 4t.js
+++ 4t.js
@@ -8,13 +8,15 @@ process.argv.splice( 0, 2 )
 process.title = '4t'
 
 /**
- * @type {Promise|{command:{[p:object]:object}}}
+ * @type {Promise | {command:{test:{file: {filename: string, print: string}}}}}
  */
 const digestive = await entry_point( process.argv )
 
+// eslint-disable-next-line default-case
 switch ( Object.entries( digestive.command )[ 0 ][ 0 ] ) {
     
-    case  'file':
+    case  'test':
+        
         ( ( options ) => {console.trace( options )} )( digestive.command.test )
         break
 }
