521d22a9aa17e9c328a0c17da2f4ddc5b6850faf
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-02-19 00:24:18 +0000

    v0.0.2 added public exports

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import {
     entry_point__,
+    file__,
+    options__,
     process_exit__,
     process_title__,
+    processor__,
     shell_exit_codes__,
-    stderr__
+    stderr__,
+    twd__,
 } from './lib/exporter.js'
 
 // - Object [activity]      ____
@@ -47,12 +51,12 @@ export const shell_exit_codes = shell_exit_codes__
  * Its return an object from the given process.argv.
  * Object [input.process_title] elaborates commands, flags and options dispatching to the right process switching the process.title.
  *
- * @param {string[]} args - The process.argv passed to the process.
+ * @param {string[]} argv - The process.argv passed to the process.
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Generator/return#using_return
  * @returns {Promise | object} - return the return of the async function generator to set it "done" and free resources.
  */
-export async function entry_point( args ) {
-    return entry_point__( args )
+export async function entry_point( argv ) {
+    return entry_point__( argv )
 }
 
 /**
@@ -60,8 +64,45 @@ export async function entry_point( args ) {
  * The returned Promise always resolves.
  *
  * @param {{object:{[p: string]: any}, keys:string[]}} process_parsed_argv - Object.
- * @returns {Promise<{command: {[p:object]:any}}>}
+ * @returns {AsyncGenerator}
  */
 export async function process_title( process_parsed_argv ) {
     return process_title__( process_parsed_argv )
+}
+
+/**
+ * Object [ input.argv ] parses the process.argv string[] and returns an object.
+ *
+ * @param { string[] } argv - the given process.argv.
+ * @returns { Promise<{ object:{ [ p: string ]: any }, keys:string[] }> }
+ */
+export async function processors( argv ){
+    return processor__( argv )
+}
+
+/**
+ * Object [ input.options ] parses, if any, the options given to the flag.
+ *
+ * @param { string }flag_value - the flag value passed to cli.
+ * @returns { Promise<{ [ p:string ]:{ [ p:string ], any} }> }
+ */
+export async function options( flag_value ){
+    return options__( flag_value )
+}
+
+/**
+ * Object [ tttt.twd ].
+ *
+ * @type {{set: ((function(string): AsyncGenerator<any, void, any>)), get(): Promise<string>}}
+ */
+export const twd = twd__
+
+/**
+ * Object [ tttt.file ] loads the given assertion file and runs the test.
+ *
+ * @param {string} filename - the file to be run.
+ * @returns {Promise<void>}
+ */
+export function file( filename ){
+    return file__( filename )
 }
