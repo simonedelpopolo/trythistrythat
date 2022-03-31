9aebe71c41bd7599dc04fe43be0a0115397a2c5f
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-03-31 21:46:58 +0000

    v1.0.1 renamed unit-test files
    ADDED try.this.sub-directory.test

---
+++ assertions/sub-directory/try.this.sub-directory.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable jsdoc/require-jsdoc */
+import * as tttt from '../../tttt.js'
+
+export default async () => {
+    
+    const error = await tttt.oki( async () => {
+        
+        tttt.describe( 'sub directory crawling UNIT test', 0, '\n' )
+        
+        return {
+            expected: 'string',
+            actual: 'string',
+            error: 'error encountered'
+        }
+        
+    } )
+    
+    if( error instanceof Error ) {
+        
+        tttt.failed( true )
+        tttt.describe( 'test failed'.red() )
+        tttt.describe( error )
+        
+    }
+    else tttt.describe( 'test passed'.green() )
+    
+    await tttt.end_test( tttt.id() )
+}
---
+++ assertions/try.this.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable jsdoc/require-jsdoc */
+import * as tttt from '../tttt.js'
+
+export default async () => {
+    
+    const error = await tttt.deeeeepStrictEqual( async () => {
+        
+        tttt.describe( 'deepStrictEqual TEST.'.green(), 'statement ->'.red(), 0, '\n' )
+        
+        return {
+            expected: [ 10 ],
+            actual: [ 10 ],
+            error: 'error encountered'
+        }
+        
+    } )
+    
+    if( error instanceof Error ) {
+        
+        tttt.failed( true )
+        tttt.describe( 'test failed'.red() )
+        tttt.describe( error )
+        
+    }
+    else tttt.describe( 'test passed'.green() )
+    
+    await tttt.end_test( tttt.id() )
+}
