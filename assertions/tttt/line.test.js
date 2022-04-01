3ae10e4cb2371df4cd196c62a8da18b86e2dd2f0
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-01 23:13:45 +0000

    v1.2.1 tttt.line() console.log() many times indicated with repeat argument

--- assertions/tttt/line.test.js
+++ assertions/tttt/line.test.js
@@ -3,7 +3,7 @@ import * as tttt from '../../tttt.js'
 
 export default async function(){
     tttt.describe( 'start' )
-    await tttt.line( '10' )
+    tttt.line( 10 )
     tttt.describe( 'end' )
 
     tttt.end_test( tttt.id() )
