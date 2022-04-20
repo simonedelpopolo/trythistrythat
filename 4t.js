e04b494f6d6f9876eb5f52573c7fe560ea6c1433
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-20 13:12:26 +0000

    v1.8.2 fixed main field to index.js

--- 4t.js
+++ 4t.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const whole_timer = process.hrtime.bigint()
+const overall_execution_time_start = process.hrtime.bigint()
 import { access } from 'node:fs/promises'
 import add from './lib/tttt/add.js'
 import { describe } from './index.js'
@@ -76,4 +76,4 @@ else{
 }
 
 
-process.on( 'exit', () => describe( 'overall time'.yellow(), '|'.black(), String( Number( process.hrtime.bigint() - whole_timer ) / 1_000_000 + 'ms' ).red() ) )
+process.on( 'exit', () => describe( 'overall execution time'.yellow(), '|'.black(), String( Number( process.hrtime.bigint() - overall_execution_time_start ) / 1_000_000 + 'ms' ).red() ) )
